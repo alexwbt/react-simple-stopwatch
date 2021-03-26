f10519e4095dbb5a5fea35794c6e7831093d9fbc
Alex Wong <alexwbtg@gmail.com>
2021-03-26 21:06:54 +0800

    updated

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,30 +1,63 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`text-align: center;user-select: none;`;
-const Display = styled.div`font-size: 50px;padding: 10px;`;
-const Laps = styled.div`font-size:30px;color:grey;padding:5px;`;
+const Container = styled.div`
+    text-align: center;
+    user-select: none;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: ${props => props.width ? `${props.width}px` : '100vw'};
+    height: ${props => props.height ? `${props.height}px` : '100vh'};
+`;
+
+const Display = styled.div`
+    font-size: 50px;
+    flex: 1;
+    position: relative;
+    > div {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+`;
+
+const Laps = styled.div`
+    flex: 1;
+    max-height: 50%;
+    overflow: auto;
+    > div {
+        font-size: 30px;
+        color: grey;
+        padding: 5px;
+    }
+`;
+
 const Button = styled.div`
-display:inline-block;
-width:200px;
-margin:10px;
-padding:10px;
-font-size:20px;
-border-radius:10px;
-border:white 1px solid;
-:hover {cursor:pointer;border:lightgrey 1px solid;}
+    display: inline-block;
+    width: 200px;
+    margin: 10px;
+    padding: 10px;
+    font-size: 20px;
+    border-radius: 10px;
+    border: white 1px solid;
+    :hover {
+        cursor: pointer;
+        border: lightgrey 1px solid;
+    }
 `;
 
 const getTimeString = time => {
-    const miliseconds = time % 1000;
+    const milliseconds = time % 1000;
     const seconds = Math.floor(time / 1000);
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
-    const strMiliseconds = `${miliseconds}`.padEnd(3, '0');
+    const strMilliseconds = `${milliseconds}`.padEnd(3, '0');
     const strSeconds = `${seconds % 60 % 60}`.padStart(2, '0');
     const strMinutes = `${minutes % 60}`.padStart(2, '0');
     const strHours = `${hours}`.padStart(2, '0');
-    return `${strHours}:${strMinutes}:${strSeconds}.${strMiliseconds}`;
+    return `${strHours}:${strMinutes}:${strSeconds}.${strMilliseconds}`;
 };
 
 const initialState = {
@@ -38,40 +71,28 @@ const initialState = {
 
 var intervalId;
 
-const Stopwatch = () => {
+const Stopwatch = ({ width, height }) => {
     const [state, setState] = useState(initialState);
 
     useEffect(() => {
-        if (intervalId) clearInterval(intervalId);
+        clearInterval(intervalId);
         intervalId = setInterval(() => {
             if (state.startPauseTime > 0) return;
-            if (!state.running) {
-                setState(prev => ({ ...prev, time: 0 }));
-                return;
-            }
-            setState(prev => ({
-                ...prev,
-                time: Date.now() - state.startTime
-            }));
+            if (!state.running) setState(prev => ({ ...prev, time: 0 }));
+            else setState(prev => ({ ...prev, time: Date.now() - state.startTime }));
         });
+        return () => clearInterval(intervalId);
     }, [state.running, state.startPauseTime, state.startTime]);
 
     const start = useCallback(() => {
-        if (state.running)
-            setState(initialState);
-        else setState(prev => ({
-            ...prev,
-            running: true,
-            startTime: Date.now(),
-        }));
+        if (state.running) setState(initialState);
+        else setState(prev => ({ ...prev, running: true, startTime: Date.now(), }));
     }, [state.running]);
 
     const lap = useCallback(() => {
         if (!state.running || state.startPauseTime > 0) return;
-        setState(prev => ({
-            ...prev,
-            laps: prev.laps.concat(Date.now() - state.startTime - state.pausedTime - prev.laps.reduce((a, b) => a + b, 0))
-        }));
+        const deltaTime = Date.now() - state.startTime - state.pausedTime;
+        setState(prev => ({ ...prev, laps: prev.laps.concat(deltaTime - prev.laps.reduce((a, b) => a + b, 0)) }));
     }, [state.running, state.startPauseTime, state.startTime, state.pausedTime]);
 
     const pause = useCallback(() => {
@@ -84,12 +105,14 @@ const Stopwatch = () => {
         else setState(prev => ({ ...prev, startPauseTime: Date.now() }));
     }, [state.running, state.startPauseTime]);
 
-    return <Container>
-        <Display>{getTimeString(state.time - state.pausedTime)}</Display>
-        <Button onClick={start}>{state.running ? 'Stop' : 'Start'}</Button>
-        <Button onClick={lap}>Lap</Button>
-        <Button onClick={pause}>{state.startPauseTime > 0 ? 'Unpause' : 'Pause'}</Button>
-        <div>{state.laps.map((time, i) => <Laps key={i}>{getTimeString(time)}</Laps>)}</div>
+    return <Container {...{ width, height }}>
+        <Display><div>{getTimeString(state.time - state.pausedTime)}</div></Display>
+        <div>
+            <Button onClick={start}>{state.running ? 'Stop' : 'Start'}</Button>
+            <Button onClick={lap}>Lap</Button>
+            <Button onClick={pause}>{state.startPauseTime > 0 ? 'Unpause' : 'Pause'}</Button>
+        </div>
+        <Laps>{state.laps.slice().reverse().map((time, i, a) => <div key={i}>{`${a.length - i}. ${getTimeString(time)}`}</div>)}</Laps>
     </Container>;
 };
 
